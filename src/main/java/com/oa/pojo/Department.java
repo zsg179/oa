047832acbb7d51a5e59c07db895290a4d6ecaf0d
ZhuShuguang <18977330642@163.com>
2019-07-30 17:55:20 +0800

    增加了Department实体类

---
+++ src/main/java/com/oa/pojo/Department.java
@@ -0,0 +1,14 @@
+package com.oa.pojo;
+
+public class Department {
+	private String deptName;//ou
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+}
