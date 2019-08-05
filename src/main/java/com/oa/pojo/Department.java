a618d5453f71602a28868ed564e8e9a9e599688d
ZhuShuguang <18977330642@163.com>
2019-08-05 11:51:19 +0800

    修改了部门实体类

--- src/main/java/com/oa/pojo/Department.java
+++ src/main/java/com/oa/pojo/Department.java
@@ -2,31 +2,37 @@ package com.oa.pojo;
 
 public class Department {
 	private String deptName;//ou
-	private Long id;//description
-	private Long parentId;//businessCategory
+	private String id;//description
+	private String parentId;//businessCategory
+	private String isParent;//st
 	public String getDeptName() {
 		return deptName;
 	}
 	public void setDeptName(String deptName) {
 		this.deptName = deptName;
 	}
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
-	public Long getParentId() {
+	public String getParentId() {
 		return parentId;
 	}
-	public void setParentId(Long parentId) {
+	public void setParentId(String parentId) {
 		this.parentId = parentId;
+	}
+	public String getIsParent() {
+		return isParent;
+	}
+	public void setIsParent(String isParent) {
+		this.isParent = isParent;
 	}
 	@Override
 	public String toString() {
-		return "Department [deptName=" + deptName + ", id=" + id + ", parentId=" + parentId + "]";
+		return "Department [deptName=" + deptName + ", id=" + id + ", parentId=" + parentId + ", isParent=" + isParent
+				+ "]";
 	}
-	
-	
 	
 }
