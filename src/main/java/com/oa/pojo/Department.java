470df8215eed6336ee63da7830417bd96c823ca0
ZhuShuguang <18977330642@163.com>
2019-08-05 09:34:10 +0800

    修改了department实体类

--- src/main/java/com/oa/pojo/Department.java
+++ src/main/java/com/oa/pojo/Department.java
@@ -2,18 +2,30 @@ package com.oa.pojo;
 
 public class Department {
 	private String deptName;//ou
-
+	private String id;//description
+	private String parentId;//businessCategory
 	public String getDeptName() {
 		return deptName;
 	}
-
 	public void setDeptName(String deptName) {
 		this.deptName = deptName;
 	}
-
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
 	@Override
 	public String toString() {
-		return "Department [deptName=" + deptName + "]";
+		return "Department [deptName=" + deptName + ", id=" + id + ", parentId=" + parentId + "]";
 	}
+	
 	
 }
