a6b6002bd6ff230488b280980d6f0aa92cf99966
ZhuShuguang <18977330642@163.com>
2019-08-05 11:52:47 +0800

    修改了页面控制类

--- src/main/java/com/oa/controller/PageController.java
+++ src/main/java/com/oa/controller/PageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 	@RequestMapping("/")
-	private String showIndex() {
+	public String showIndex() {
 		return "index";
 	}
 
