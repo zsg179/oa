887be1241ef6d5938baa4dedc335d36f8aaee2cc
ZhuShuguang <18977330642@163.com>
2019-08-05 17:27:40 +0800

    修改了部门的dao实现，完成了树的显示

--- src/main/java/com/oa/dao/impl/DeptDaoImpl.java
+++ src/main/java/com/oa/dao/impl/DeptDaoImpl.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Repository;
 
 import com.oa.dao.DeptDao;
 import com.oa.mapper.DepartmentAttributeMapper;
+import com.oa.mapper.PersonAttributeMapper;
 import com.oa.pojo.Department;
 import com.oa.pojo.EasyUITreeNote;
+import com.oa.pojo.Employee;
+
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 
 /**
@@ -20,7 +23,7 @@ import static org.springframework.ldap.query.LdapQueryBuilder.query;
  *
  */
 
-@Repository
+@Repository(value = "deptDao")
 public class DeptDaoImpl implements DeptDao {
 	@Autowired
 	private LdapTemplate ldapTemplate;
@@ -31,11 +34,30 @@ public class DeptDaoImpl implements DeptDao {
 				new DepartmentAttributeMapper());
 		List<EasyUITreeNote> result = new ArrayList<>();
 		for (Department department : list) {
-			EasyUITreeNote note = new EasyUITreeNote();
-			note.setId(Long.parseLong(department.getId()));
-			note.setText(department.getDeptName());
-			note.setState(department.getIsParent().equals("1") ? "closed" : "open");
-			result.add(note);
+			
+			// 取id
+			String id = department.getId();
+			if (department.getDeptName() == null) {
+				// 查询到的是员工
+				List<Employee> personList = ldapTemplate.search(query().where("businessCategory").is(department.getParentId()),
+						new PersonAttributeMapper());
+				for (Employee emp : personList) {
+					EasyUITreeNote note = new EasyUITreeNote();
+					// 补全pojo
+					note.setId(Long.parseLong(emp.getId()));
+					note.setText(emp.getFullName());
+					note.setState("open");
+					result.add(note);
+				}
+				break;
+			} else {
+				EasyUITreeNote note = new EasyUITreeNote();
+				// 部门
+				note.setId(Long.parseLong(id));
+				note.setText(department.getDeptName());
+				note.setState(department.getIsParent().equals("1") ? "closed" : "open");
+				result.add(note);
+			}		
 		}
 		return result;
 	}
