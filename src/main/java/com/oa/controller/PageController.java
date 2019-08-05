45918cd5dd5a579166f0e2294b3a0299405551d4
ZhuShuguang <18977330642@163.com>
2019-08-05 11:44:39 +0800

    新增了页面控制层

---
+++ src/main/java/com/oa/controller/PageController.java
@@ -0,0 +1,29 @@
+package com.oa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/* 
+* @author 朱树广 
+* @date 2019年8月5日 上午10:01:59 
+* @version 1.0 
+*/
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	private String showIndex() {
+		return "index";
+	}
+
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page) {
+		return page;
+	}
+
+	@RequestMapping("/rest/page/{page}")
+	public String showEditPage(@PathVariable String page) {
+		return page;
+	}
+}
+
