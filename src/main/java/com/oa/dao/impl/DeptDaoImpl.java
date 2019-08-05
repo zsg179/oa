0d239ff1fcf180f784b7f1d6968b75f28fbf60e7
ZhuShuguang <18977330642@163.com>
2019-08-05 11:52:11 +0800

    修改了部门dao层和实现

--- src/main/java/com/oa/dao/impl/DeptDaoImpl.java
+++ src/main/java/com/oa/dao/impl/DeptDaoImpl.java
@@ -1,7 +1,43 @@
 package com.oa.dao.impl;
 
-import com.oa.dao.DeptDao;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.oa.dao.DeptDao;
+import com.oa.mapper.DepartmentAttributeMapper;
+import com.oa.pojo.Department;
+import com.oa.pojo.EasyUITreeNote;
+import static org.springframework.ldap.query.LdapQueryBuilder.query;
+
+/**
+ * 部门dao的实现类
+ * 
+ * @author 朱树广
+ *
+ */
+
+@Repository
 public class DeptDaoImpl implements DeptDao {
+	@Autowired
+	private LdapTemplate ldapTemplate;
+
+	@Override
+	public List<EasyUITreeNote> getDeptList(String parentId) {
+		List<Department> list = ldapTemplate.search(query().where("businessCategory").is(parentId),
+				new DepartmentAttributeMapper());
+		List<EasyUITreeNote> result = new ArrayList<>();
+		for (Department department : list) {
+			EasyUITreeNote note = new EasyUITreeNote();
+			note.setId(Long.parseLong(department.getId()));
+			note.setText(department.getDeptName());
+			note.setState(department.getIsParent().equals("1") ? "closed" : "open");
+			result.add(note);
+		}
+		return result;
+	}
 
 }
