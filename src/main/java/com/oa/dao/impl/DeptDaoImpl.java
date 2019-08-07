59f9ea81eacd28fbc9c282c9a34bab7089f73171
ZhuShuguang <18977330642@163.com>
2019-08-07 10:37:34 +0800

    完成了部门查询功能，新增了buildDn等方法，未完善

--- src/main/java/com/oa/dao/impl/DeptDaoImpl.java
+++ src/main/java/com/oa/dao/impl/DeptDaoImpl.java
@@ -3,16 +3,24 @@ package com.oa.dao.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Name;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.support.LdapNameBuilder;
+import org.springframework.ldap.support.LdapUtils;
 import org.springframework.stereotype.Repository;
 
 import com.oa.dao.DeptDao;
 import com.oa.mapper.DepartmentAttributeMapper;
 import com.oa.mapper.PersonAttributeMapper;
 import com.oa.pojo.Department;
+import com.oa.pojo.EasyUIDataGridResult;
 import com.oa.pojo.EasyUITreeNote;
 import com.oa.pojo.Employee;
+import com.oa.util.OAResult;
 
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 
@@ -28,19 +36,40 @@ public class DeptDaoImpl implements DeptDao {
 	@Autowired
 	private LdapTemplate ldapTemplate;
 
+	public static final String BASE_DN = "dc=poke_domain,dc=com";
+
+	protected Name buildDn(Department dept) {
+		return LdapNameBuilder.newInstance(BASE_DN).add("o", dept.getO()).add("ou", dept.getDeptName()).build();
+	}
+
+	protected Department buildDept(Name dn, Attributes attrs) {
+		Department dept = new Department();
+		// dept.setO(LdapUtils.getStringValue(dn, "o"));
+		dept.setDeptName(LdapUtils.getStringValue(dn, "ou"));
+		try {
+			dept.setO((String) attrs.get("l").get());
+			dept.setId((String) attrs.get("description").get());
+			dept.setParentId((String) attrs.get("businessCategory").get());
+			dept.setIsParent((String) attrs.get("st").get());
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dept;
+	}
+
 	@Override
 	public List<EasyUITreeNote> getDeptList(String parentId) {
 		List<Department> list = ldapTemplate.search(query().where("businessCategory").is(parentId),
 				new DepartmentAttributeMapper());
 		List<EasyUITreeNote> result = new ArrayList<>();
 		for (Department department : list) {
-			
 			// 取id
 			String id = department.getId();
-			if (department.getDeptName() == null) {
+			if (department.getIsParent().equals("0")) {
 				// 查询到的是员工
-				List<Employee> personList = ldapTemplate.search(query().where("businessCategory").is(department.getParentId()),
-						new PersonAttributeMapper());
+				List<Employee> personList = ldapTemplate.search(
+						query().where("businessCategory").is(department.getParentId()), new PersonAttributeMapper());
 				for (Employee emp : personList) {
 					EasyUITreeNote note = new EasyUITreeNote();
 					// 补全pojo
@@ -57,8 +86,37 @@ public class DeptDaoImpl implements DeptDao {
 				note.setText(department.getDeptName());
 				note.setState(department.getIsParent().equals("1") ? "closed" : "open");
 				result.add(note);
-			}		
+			}
 		}
+		return result;
+	}
+
+	@Override
+	public OAResult create(Department dept) {
+		Name dn = buildDn(dept);
+		return null;
+	}
+
+	@Override
+	public OAResult delete(Department dept) {
+		Name dn = buildDn(dept);
+		ldapTemplate.unbind(dn);
+		return OAResult.ok();
+	}
+
+	@Override
+	public OAResult edit(Department dept) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EasyUIDataGridResult geteDeptInfoById(String id) {
+		List<Department> list = ldapTemplate.search(query().where("description").is(id),
+				new DepartmentAttributeMapper());
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		result.setTotal(list.size());
 		return result;
 	}
 
