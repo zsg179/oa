625db74c3c8fcdd6a80a7311c39edef1caee3705
ZhuShuguang <18977330642@163.com>
2019-08-12 10:32:14 +0800

    新增了员工管理的后端接口和实现

--- src/main/java/com/oa/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/com/oa/dao/impl/EmployeeDaoImpl.java
@@ -3,7 +3,34 @@ package com.oa.dao.impl;
 import org.springframework.stereotype.Repository;
 
 import com.oa.dao.EmployeeDao;
-@Repository
+import com.oa.pojo.EasyUIDataGridResult;
+import com.oa.pojo.Employee;
+import com.oa.util.OAResult;
+@Repository(value = "employeeDao")
 public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Override
+	public OAResult create(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OAResult delete(String ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OAResult edit(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EasyUIDataGridResult getEmpInfoById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/oa/service/EmployeeService.java
+++ src/main/java/com/oa/service/EmployeeService.java
@@ -1,5 +1,33 @@
 package com.oa.service;
 
-public interface EmployeeService {
+import com.oa.pojo.EasyUIDataGridResult;
+import com.oa.pojo.Employee;
+import com.oa.util.OAResult;
 
+public interface EmployeeService {
+	/**
+	 * 新增员工
+	 * @param emp
+	 * @return
+	 */
+	OAResult create(Employee emp);
+	
+	/**
+	 * 删除员工
+	 * @param ids
+	 * @return
+	 */
+	OAResult delete(String ids);
+	/**
+	 * 编辑员工
+	 * @param emp
+	 * @return
+	 */
+	OAResult edit(Employee emp);
+	/**
+	 * 通过id获取员工信息
+	 * @param id
+	 * @return
+	 */
+	EasyUIDataGridResult getEmpInfoById(String id);
 }
